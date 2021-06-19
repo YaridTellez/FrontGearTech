fc068b2d4f4df1f3d5b23267329e55fc88028a9e
Yarid Tellez <riutellez@gmail.com>
2021-06-19 14:18:28 -0500

    initial componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
+import { FooterComponent } from './Views/footer/footer.component';
+import { HeaderComponent } from './Views/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
